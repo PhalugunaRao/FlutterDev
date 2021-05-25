507791938a2665b43439bcf598e1e455e2330f2d
PhalgunaRao <phalu900@gmail.com>
2021-05-25 07:12:31 +0530

    custom styles and font style

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,25 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('my first app'),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
     ),
     body:Center(
-      child: Text('hello Geethu'),
+      child: Text(
+          'hello Geethu',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+        ),
+      ),
     ),
 
    floatingActionButton: FloatingActionButton(
+     onPressed: (){},
       child: Text('OK'),
+     backgroundColor: Colors.red[600],
 
   ),
   ),
