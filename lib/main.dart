24a1e6a430684323a24048ff242d4c8d99c251ce
PhalgunaRao <phalu900@gmail.com>
2021-05-26 21:57:46 +0530

    Images

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body:Center(
-      child: Text(
-          'hello Geethu',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower',
-        ),
-      ),
-    ),
-
-   floatingActionButton: FloatingActionButton(
-     onPressed: (){},
-      child: Text('OK'),
-     backgroundColor: Colors.red[600],
-
-  ),
-  ),
+  home:Home(),
 
 ));
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body:Center(
+        //Network Image
+        child: Image.asset('assets/demo.png'),
+
+       /* child: Image(
+          image: AssetImage('assets/demo.png'),
+           //Image.network(src)
+          //image: NetworkImage('https://thumbs.dreamstime.com/z/nature-forest-trees-growing-to-upward-to-sun-wallpaper-42907586.jpg'),
+
+        ),*/
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text('Save'),
+        backgroundColor: Colors.red[600],
+
+      ),
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
