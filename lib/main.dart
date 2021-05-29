2b07d47d857fe28c5d24ff9f0a5c09a4452b5ca3
PhalgunaRao <phalu900@gmail.com>
2021-05-29 19:27:42 +0530

    Id card Ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,97 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home:Home(),
+  home:NinjaCard(),
 
 ));
 
+class NinjaCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Ninja ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/demo.png'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+            Text('NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'PhalgunaRao',
+              style: TextStyle(
+                color: Colors.amber,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text('CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '8',
+              style: TextStyle(
+                  color: Colors.amber,
+                  letterSpacing: 2.0,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              children:<Widget> [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                    'phaguna@yopmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
 class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
@@ -14,13 +101,15 @@ class Home extends StatelessWidget{
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      /*body:Container(
+      */
+/*body:Container(
         padding: EdgeInsets.symmetric(horizontal: 30.0,vertical: 10.0),
         //padding: EdgeInsets.all(30.0),
         margin: EdgeInsets.all(30.0),
         color: Colors.grey[400],
         child: Text('hello') ,
-      ),*/
+      ),*//*
+
 
       body:Row(
         children: [
@@ -85,4 +174,4 @@ class Home extends StatelessWidget{
 
 
 
-}
+}*/
