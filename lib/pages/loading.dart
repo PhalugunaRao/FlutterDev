7cf06b0c4780ff479ef2c1bcc765826bac5e989e
PhalgunaRao <phalu900@gmail.com>
2021-06-05 22:31:53 +0530

    Time network call

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -10,18 +10,24 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async{
-   Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
-   //print(response.body);
+  void getTime() async{
+   Response response = await get('http://worldtimeapi.org/api/timezone/europe/london');
    Map data = jsonDecode(response.body);
    print(data);
-   print(data['title']);
+
+   String datetime=data['datetime'];
+   String offset = data['utc_offset'].substring(1,3);
+
+   //create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+   print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
 
   }
   @override
